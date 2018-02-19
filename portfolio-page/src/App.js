8c1d7034713d76fd4607fdf7db298875aa5f0dff
c0nniewang <connie.wang303@gmail.com>
2018-02-19 15:02:24 -0500

    imported fonts

--- portfolio-page/src/App.js
+++ portfolio-page/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Sidebar />
+        <p className="App-title">
+          Hi, I'm Connie Wang.
+        </p>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          I am a full stack web developer currently based in NYC looking for my next adventure.
         </p>
       </div>
     );
---
+++ portfolio-page/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <p className="sidebar-menu">about</p>
+      </div>
+    )
+  }
+}
+
+export default Sidebar
