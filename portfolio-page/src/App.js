8843d5d4463f99e62e069b15c7c32a8a75b2a539
c0nniewang <connie.wang303@gmail.com>
2018-02-19 18:09:37 -0500

    set up functionality for sidebar navigation

--- portfolio-page/src/App.js
+++ portfolio-page/src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 import Sidebar from './components/Sidebar'
 
 class App extends Component {
+  state = {activeItem: 'home'}
+
+  // let display;
+
+  handleClick = (ev) => {
+    console.log(ev.target.id)
+
+    this.setState({
+      activeItem: ev.target.id
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <header></header>
-        <Sidebar />
+        <Sidebar handleClick={this.handleClick} />
         <main>
           <p className="App-title">
             Hi, I'm Connie Wang.
           </p>
           <p className="App-intro">
-            I am a full stack web developer currently based in NYC looking for my next adventure. After playing piano for 18 years, I decided to jump into something new and try my hand at coding. 
+            I am a full stack web developer currently based in NYC looking for my next adventure. I recently completed the 15-week Software Engineering Immersive program @ Flatiron School. 
           </p>
         </main>
         <footer></footer>
--- portfolio-page/src/components/Sidebar.js
+++ portfolio-page/src/components/Sidebar.js
@@ -2,13 +2,34 @@ import React from 'react'
 
 class Sidebar extends React.Component {
   render() {
+    console.log(this.props)
     return (
       <div className="sidebar">
-        <button id="sidebar-menu">linkedin</button>
-        <button id="sidebar-menu">medium</button>
-        <button id="sidebar-menu">github</button>
-        <button id="sidebar-menu">projects</button>
-        <button id="sidebar-menu">about</button>
+        <div
+        className="button" 
+        onClick={(e) => this.props.handleClick(e)}
+        id="linkedin"
+        >linkedin</div>
+        <div 
+        className="button" 
+        id="medium"
+        onClick={(ev) => this.props.handleClick(ev)}
+        >medium</div>
+        <div 
+        className="button" 
+        id="github"
+        onClick={(ev) => this.props.handleClick(ev)}
+        >github</div>
+        <div 
+        className="button" 
+        id="projects"
+        onClick={(ev) => this.props.handleClick(ev)}
+        >projects</div>
+        <div 
+        className="button"
+        id="about" 
+        onClick={(ev) => this.props.handleClick(ev)}
+        >about</div>
       </div>
     )
   }
